012bf5faf5264953301854798250a3baa9f6c38f
Pavlos Panagiotidis <ppa@covis.de>
2018-07-02 19:44:08 +0200

    initial routing attempt

---
+++ src/Map.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class Map extends Component {
+    render() {
+        return (
+            <h1>This is not the map you are looking for.</h1>
+        );
+    }
+}
+
+export default Map;
